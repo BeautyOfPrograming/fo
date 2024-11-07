00989592f997911a076aaf3f765f223ac1c66dce
BeautyOfPrograming <aliahmadmohammadi412@gmail.com>
2024-11-08 00:13:36 +0330

    1403/08/17.2

--- app/src/main/java/com/example/fragment4/Fragment_Profile.java
+++ app/src/main/java/com/example/fragment4/Fragment_Profile.java
@@ -34,6 +34,7 @@ public class Fragment_Profile extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.profile_fragment, container, false);
 
